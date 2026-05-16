5acf4d69fc8956fcf46d7c446321abed596b39dd
epica <epicarusracoma@gmail.com>
2026-05-17 01:44:42 +0800

    environment fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://your-live-backend-url.onrender.com' // TODO: Replace with your actual Render backend URL
+    apiUrl: 'https://node-mysql-api-yes5.onrender.com' // TODO: Replace with your actual Render backend URL
 };
 
 export {};
